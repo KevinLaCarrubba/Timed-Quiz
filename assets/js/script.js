282befe9fde67782dcff6b949eaca1247ad8449e
Kevin LaCarrubba <Kevin.LaCarrubba@yahoo.com>
2021-08-09 19:43:22 -0400

    lay out some css and html. added questions to js file

---
+++ assets/js/script.js
@@ -0,0 +1,70 @@
+//1. Set up the questions in number order.
+questions = [
+  {
+    num: 1,
+    question: "How many times have the olympics been hosted in the USA ?",
+    answer: "8",
+    options: ["5", "7", "8", "12"],
+  },
+  {
+    num: 2,
+    question:
+      "What was the most gold medals won by the USA at 1 olympic games ?",
+    answer: "83",
+    options: ["78", "83", "44", "72"],
+  },
+  {
+    num: 3,
+    question:
+      "How many times has the USA won the most gold medals at the summer olympics ?",
+    answer: "18",
+    options: ["12", "15", "9", "18"],
+  },
+  {
+    num: 4,
+    question:
+      "When was the last time USA did not win the most total medals in the winter olympics ?",
+    answer: "2018",
+    options: ["2018", "2002", "2010", "1998"],
+  },
+  {
+    num: 5,
+    question: "What sport does the USA have the most gold medals in ?",
+    answer: "Athletics",
+    options: ["Shooting", "Athletics", "Swimming", "Basketball"],
+  },
+  {
+    num: 6,
+    question:
+      "What was the most gold medals won by the USA at 1 olympic games ?",
+    answer: "83",
+    options: ["78", "83", "44", "72"],
+  },
+  {
+    num: 7,
+    question:
+      "What is the highest medal count by a single American athlete at the olympics ?",
+    answer: "28",
+    options: ["19", "12", "28", "18"],
+  },
+  {
+    num: 8,
+    question:
+      "What was the most gold medals won by the USA at 1 olympic games ?",
+    answer: "83",
+    options: ["78", "83", "44", "72"],
+  },
+  {
+    num: 9,
+    question:
+      "When was the last time the olympic games were held in the USA ? ",
+    answer: "2002",
+    options: ["2002", "1996", "1980", "2020"],
+  },
+  {
+    num: 10,
+    question: "When are the olympic games planned to be in USA again ?",
+    answer: "2028",
+    options: ["2024", "2028", "2032", "2022"],
+  },
+];
