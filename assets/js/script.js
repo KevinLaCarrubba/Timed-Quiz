a9b73f34d62309c15648707d6fe56b656f920283
Kevin LaCarrubba <Kevin.LaCarrubba@yahoo.com>
2021-08-10 23:22:48 -0400

    assigned question and option values. display questions and options.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,151 @@
 //1. Set up the questions in number order.
-questions = [
+var questions = [
   {
-    num: 1,
     question: "How many times have the olympics been hosted in the USA ?",
-    answer: "8",
     options: ["5", "7", "8", "12"],
+    answer: "8",
   },
   {
-    num: 2,
     question:
       "What was the most gold medals won by the USA at 1 olympic games ?",
-    answer: "83",
     options: ["78", "83", "44", "72"],
+    answer: "83",
   },
   {
-    num: 3,
     question:
       "How many times has the USA won the most gold medals at the summer olympics ?",
-    answer: "18",
     options: ["12", "15", "9", "18"],
+    answer: "18",
   },
   {
-    num: 4,
     question:
       "When was the last time USA did not win the most total medals in the winter olympics ?",
-    answer: "2018",
     options: ["2018", "2002", "2010", "1998"],
+    answer: "2018",
   },
   {
-    num: 5,
     question: "What sport does the USA have the most gold medals in ?",
-    answer: "Athletics",
     options: ["Shooting", "Athletics", "Swimming", "Basketball"],
+    answer: "Athletics",
   },
   {
-    num: 6,
     question:
       "What was the most gold medals won by the USA at 1 olympic games ?",
-    answer: "83",
     options: ["78", "83", "44", "72"],
+    answer: "83",
   },
   {
-    num: 7,
     question:
       "What is the highest medal count by a single American athlete at the olympics ?",
-    answer: "28",
     options: ["19", "12", "28", "18"],
+    answer: "28",
   },
   {
-    num: 8,
     question:
       "What was the most gold medals won by the USA at 1 olympic games ?",
-    answer: "83",
     options: ["78", "83", "44", "72"],
+    answer: "83",
   },
   {
-    num: 9,
     question:
       "When was the last time the olympic games were held in the USA ? ",
-    answer: "2002",
     options: ["2002", "1996", "1980", "2020"],
+    answer: "2002",
   },
   {
-    num: 10,
     question: "When are the olympic games planned to be in USA again ?",
-    answer: "2028",
     options: ["2024", "2028", "2032", "2022"],
+    answer: "2028",
   },
 ];
+
+var timerEL = document.querySelector(".timer");
+var startBtnEl = document.querySelector("#start-button");
+
+var timeLeft = 120;
+var pointDeduction = 10;
+//Create function to start the timer
+
+//Extract set interval so that is global so it can be started or updated.
+function startTimer() {
+  var timerInterval = setInterval(function () {
+    timeLeft--;
+    timerEL.textContent = timeLeft + " seconds left";
+
+    if (timeLeft === 0) {
+      clearInterval(timerInterval);
+    }
+  }, 1000);
+}
+//Create Function To Check Answers
+//If right go to next question
+//If wrong pointDeduction
+//Create Function to End timer
+//Create function to calculate score
+//Add high score to high score page
+
+//Create a function to start the quiz.
+function startQuiz() {
+  document.getElementById("start-button").style.display = "none";
+  startTimer();
+}
+
+startBtnEl.addEventListener("click", startQuiz);
+var quizDisplay = document.querySelector(".quiz-box");
+function renderQuiz() {
+  //Loop through array of obj.
+  questions.forEach((item, key) => {
+    //Create variable for question container
+    var questionContainer = document.createElement("div");
+    questionContainer.classList.add("questionContainer");
+    //Create variable for the question
+    var question = document.createElement("div");
+    question.classList.add("question");
+    question.textContent = item.question;
+    //Create variable for the answers
+    var answers = document.createElement("div");
+    answers.classList.add("answers");
+    //Loop through options array in item to make answer options
+    item.options.forEach((option) => {
+      var element = document.createElement("button");
+      element.classList.add("option");
+      element.textContent = option;
+      //Identifier to let you know what question youre on
+      element.dataset.question = key;
+      //Indentifier to let you know what answer was clicked
+      element.setAttribute("value", option);
+      element.addEventListener("click", checkAnswer);
+      answers.appendChild(element);
+    });
+    //Append html elements to parent container
+    questionContainer.appendChild(question);
+    questionContainer.appendChild(answers);
+    quizDisplay.appendChild(questionContainer);
+  });
+
+  // While inside the loop. create the div for the question.
+  //create the div for the options
+}
+//Grab quiz-box when the document starts
+window.addEventListener("load", renderQuiz);
+//Add an onclick function
+
+function checkAnswer(event) {
+  //check the data set question
+  var currentQuestionIndex = event.target.dataset.question;
+  //check the option
+  var currentOption = event.target.value;
+  //get the correct object out of the questions array
+  var currentQuestion = questions[currentQuestionIndex];
+  //compare the current option to the currentQuestion.answer
+  if (currentOption === currentQuestion.answer) {
+    //hide current question
+    //show next question
+    //return out
+  }
+  //if currentOption !== currentQuestion.answer
+  //deduct points
+  //hide current question
+  //show next question
+  //return out
+}
