93175f75f9a91a59f8960f550a3b547a05401490
Kevin LaCarrubba <Kevin.LaCarrubba@yahoo.com>
2021-08-11 23:48:02 -0400

    rework on getting and adding to localstorage

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -5,10 +5,7 @@ var displayHighScore = document.getElementById("high-score-page");
 var listHighScore = document.getElementById("high-scores-list");
 var clearButton = document.getElementById("clear");
 var homeButton = document.getElementById("homePage");
-var objectArray = {
-  namesArray: [],
-  scoreArray: [],
-};
+
 //create a function to open home page
 function goHomePage() {
   window.open("index.html");
@@ -27,57 +24,24 @@ clearButton.addEventListener("click", clearStorage);
 function getUserInput() {
   //save the user input value in userName
   var userName = document.querySelector("#user-name").value;
-  // save username to local storage
-  localStorage.setItem("Username", userName);
+  //dont allow user to enter a blank name
+  if (userName === "") {
+    alert("enter your name");
+    return;
+  }
+  var storedScore = localStorage.getItem("TotalScore");
+  var newUserScore = { userName: userName, score: storedScore };
+  var previousScores = localStorage.getItem("Highscores");
+  if (previousScores) {
+    var parseScores = JSON.parse(previousScores);
+    parseScores.push(newUserScore);
+    localStorage.setItem("Highscores", parseScores);
+  }
   //hide the formDisplay
   formDisplay.style.display = "none";
-  getStoredInfo();
   renderHighScore();
 }
 //add event listener to submit button
 submitButton.addEventListener("click", getUserInput);
 
-function getStoredInfo() {
-  //get name from local storage and put it in var storedName
-  var storedName = JSON.parse(localStorage.getItem("Username"));
-  //get score from local storage and put it in var storeScore
-  var storedScore = JSON.parse(localStorage.getItem("TotalScore"));
-  //push the stored name into the obj.nameArray
-  objectArray.namesArray.push(storedName);
-  //[push the stored score into obj.scoreArray
-  objectArray.scoreArray.push(storedScore);
-  //check if the obj.nameArray is empty
-  if (objectArray.namesArray !== null) {
-    objectArray.namesArray = storedName;
-  }
-  //check if the obj.scoreArray is empty
-  if (objectArray.scoreArray !== null) {
-    objectArray.scoreArray = storedScore;
-  }
-}
-
-function renderHighScore() {
-  //remove the hide class from displayHighScore
-  displayHighScore.classList.remove("hide");
-  //set it to display in inline block
-  displayHighScore.style.display = "inline=block";
-  //loop through object
-  objectArray.forEach((item, key) => {
-    var divAdd = document.createElement("div");
-    divAdd.classList.add("line" + key);
-    //loop through names
-    item.namesArray.forEach((name) => {
-      var nameLi = document.createElement("li");
-      nameLi.classList.add("nameLi");
-      nameLi.textContent = name;
-      nameLi.appendChild(divAdd);
-    });
-    //loop through score
-    item.scoreArray.forEach((score) => {
-      var scoreLi = document.createElement("li");
-      scoreLi.classList.add("scoreLi");
-      scoreLi.textContent = score;
-      scoreLi.appendChild(divAdd);
-    });
-  });
-}
+function renderHighScore() {}
